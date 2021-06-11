252526988c57440b56dc48de80a6a754eaddf8f3
Antonio Chiappetta <antonychiappetta@gmail.com>
2021-06-11 17:59:07 +0200

    Fix indentation in controller

--- eaventure-backend/sources/features/api/back-office/BackOfficeController.js
+++ eaventure-backend/sources/features/api/back-office/BackOfficeController.js
@@ -2,52 +2,52 @@ const { Keystone } = require('@keystonejs/keystone');
 const { BackOffice } = require('../back-office/BackOfficeService');
 const { utils } = require('../../../utils/writer');
 
-exports.addInterestItem = function addInterestItem (req, res, next, body) {
-    BackOffice.addInterestItem(body)
-      .then(function (response) {
-        utils.writeJson(res, response);
-      })
-      .catch(function (response) {
-        utils.writeJson(res, response);
-      });
-  };
-  
-exports.addSocialContent = function addSocialContent (req, res, next, body) {
-    BackOffice.addSocialContent(body)
-      .then(function (response) {
-        utils.writeJson(res, response);
-      })
-      .catch(function (response) {
-        utils.writeJson(res, response);
-      });
-  };
-  
-exports.addTrainStop = function addTrainStop (req, res, next, body) {
-    BackOffice.addTrainStop(body)
+module.exports.addInterestItem = function addInterestItem (req, res, next, body) {
+  BackOffice.addInterestItem(body)
     .then(function (response) {
-        utils.writeJson(res, response);
+      utils.writeJson(res, response);
     })
     .catch(function (response) {
-        utils.writeJson(res, response);
+      utils.writeJson(res, response);
     });
-  };
-  
-exports.getTrainStop = function getTrainStop (req, res, next, id) {
-    BackOffice.getTrainStop(id)
-      .then(function (response) {
-        utils.writeJson(res, response);
-      })
-      .catch(function (response) {
-        utils.writeJson(res, response);
-      });
 };
   
-  module.exports.searchTrainStops = function searchTrainStops (req, res, next, city) {
-    BackOffice.searchTrainStops(city)
-      .then(function (response) {
-        utils.writeJson(res, response);
-      })
-      .catch(function (response) {
-        utils.writeJson(res, response);
-      });
-  };
+module.exports.addSocialContent = function addSocialContent (req, res, next, body) {
+  BackOffice.addSocialContent(body)
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
+  
+module.exports.addTrainStop = function addTrainStop (req, res, next, body) {
+  BackOffice.addTrainStop(body)
+  .then(function (response) {
+      utils.writeJson(res, response);
+  })
+  .catch(function (response) {
+      utils.writeJson(res, response);
+  });
+};
+  
+module.exports.getTrainStop = function getTrainStop (req, res, next, id) {
+  BackOffice.getTrainStop(id)
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
+  
+module.exports.searchTrainStops = function searchTrainStops (req, res, next, city) {
+  BackOffice.searchTrainStops(city)
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
